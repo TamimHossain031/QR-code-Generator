37c5abea3f3e552706622b9d1cfb84fb8f9a658f
Tamim <tamimhossain031@gmail.com>
2023-12-30 00:20:06 +0600

    generate qr code

---
+++ script.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector('.btn');
+let img = document.querySelector('.qr-code');
+let input = document.querySelector('#text');
+
+btn.addEventListener('click',generate)
+
+function generate(){
+    if(input.value.length > 0){
+        img.classList.remove('show')
+        img.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+ input.value
+        img.classList.add('show')
+    }else{
+        img.classList.remove('show');
+        input.classList.add('error');
+    }
+        
+}
+
+
